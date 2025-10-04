c2ad39fe60202ba4d1ceffe07d52a07e4945e23e
Adam Michałowski <adam.michalowski@aexol.com>
2025-10-04 15:51:09 +0200

    signout callback for fb

---
+++ src/app/api/server-signout/route.ts
@@ -0,0 +1,8 @@
+import { signOut } from "@/auth";
+import { NextResponse } from "next/server";
+
+export async function GET(request: Request) {
+  await signOut({ redirect: false });
+
+  return NextResponse.redirect(new URL("/", request.url));
+}
